35d1f605dcd85e3d9df53e5969213e90857e5e26
BastienCarbonnier <bastien.carbonnier@gmail.com>
2024-02-13 15:25:07 -0500

    main: Correct service call

--- src/core/services/shortener.service.ts
+++ src/core/services/shortener.service.ts
@@ -10,6 +10,6 @@ export const shortenUrl = async (urlToShorten: string): Promise<BackendResponse>
   return (await axios.post(`${import.meta.env.VITE_URL_SHORTNER_API_URL}/url-shortener/shorten`, { urlToShorten }, { headers })).data;
 }
 
-export const getFullUrl = async (shortUrl: string): Promise<BackendResponse> => {
-  return (await axios.get(`${import.meta.env.VITE_URL_SHORTNER_API_URL}/url-shortener/`, {headers, params: { shortUrl }})).data;
+export const getFullUrl = async (shortUrlId: string): Promise<BackendResponse> => {
+  return (await axios.get(`${import.meta.env.VITE_URL_SHORTNER_API_URL}/url-shortener/`, { headers, params: { shortUrlId }})).data;
 }
